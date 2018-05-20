60868c3287f94eb28504430c814fd574a7a68d0a
Unknown <mokchend@yahoo.com>
2018-05-20 23:49:05 +0200

    startNewRound with random number

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,13 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var currentValue: Int = 50
+    var currentValue: Int = 0
+    var targetValue: Int = 0
     @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         currentValue = lroundf(slider.value)
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        targetValue = Int(arc4random_uniform(100)) + 1
+        currentValue = 50
+        slider.value = Float(currentValue)
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,11 +41,12 @@ class ViewController: UIViewController {
     
     @IBAction func showAlert() {
         print("+++ showAlert \(currentValue)" )
-        let message = "Ths value pf the slider is:\(currentValue)"
+        let message = "Ths value pf the slider is:\(currentValue)" + "\n The target value is \(targetValue)"
         let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     @IBAction func knockKnock() {
