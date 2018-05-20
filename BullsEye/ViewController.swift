fb1a1028fb6eb88cad8385e6406cdc870ce2e6d2
Unknown <mokchend@yahoo.com>
2018-05-21 00:31:39 +0200

    ScoreLabel

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     var score = 0
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,7 @@ class ViewController: UIViewController {
     
     func updateLabels() {
         targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
     }
     
     func startNewRound() {
