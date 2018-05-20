514d20a7f4e36fc5b7e619e27f0346fb788f2db3
Unknown <mokchend@yahoo.com>
2018-05-21 00:26:41 +0200

    Inference type

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var currentValue: Int = 0
-    var targetValue: Int = 0
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
 
@@ -47,8 +48,9 @@ class ViewController: UIViewController {
     
     @IBAction func showAlert() {
         print("+++ showAlert \(currentValue)" )
-        let difference: Int = abs(currentValue - targetValue)
+        let difference = abs(currentValue - targetValue)
         let points = 100 - difference
+        score += points
         
         let message = "You score \(points) points"
         
